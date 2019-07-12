2af4f22e5a16ea0d880cc7be6451b5486a590e62
qinhao <qinhao@ruigushop.com>
2019-07-12 13:59:03 +0800

    新增客服端代码

--- Server.class.php
+++ Server.class.php
@@ -2,39 +2,38 @@
 class Server
 {
     private $serv;
+
     public function __construct() {
-    $this->serv = new swoole_server("0.0.0.0", 9501);
-    $this->serv->set(array(
-    'worker_num' => 8,
-    'daemonize' => false,
-    'max_request' => 10000,
-    'dispatch_mode' => 2,
-    'debug_mode'=> 1
-    ));
+        $this->serv = new swoole_server("0.0.0.0", 9501);
+        $this->serv->set(array(
+            'worker_num' => 8,
+            'daemonize' => false,
+        ));
 
-    $this->serv->on('Start', array($this, 'onStart'));
-    $this->serv->on('Connect', array($this, 'onConnect'));
-    $this->serv->on('Receive', array($this, 'onReceive'));
-    $this->serv->on('Close', array($this, 'onClose'));
+        $this->serv->on('Start', array($this, 'onStart'));
+        $this->serv->on('Connect', array($this, 'onConnect'));
+        $this->serv->on('Receive', array($this, 'onReceive'));
+        $this->serv->on('Close', array($this, 'onClose'));
 
-    $this->serv->start();
+        $this->serv->start();
     }
 
     public function onStart( $serv ) {
-    echo "Start\n";
+        echo "Start\n";
     }
 
     public function onConnect( $serv, $fd, $from_id ) {
-    $serv->send( $fd, "Hello {$fd}!" );
+        $serv->send( $fd, "Hello {$fd}!" );
     }
 
     public function onReceive( swoole_server $serv, $fd, $from_id, $data ) {
-    echo "Get Message From Client {$fd}:{$data}\n";
+        echo "Get Message From Client {$fd}:{$data}\n";
+        $serv->send($fd, $data);
     }
 
     public function onClose( $serv, $fd, $from_id ) {
-    echo "Client {$fd} close connection\n";
+        echo "Client {$fd} close connection\n";
     }
 }
-// 启动服务器
+// 启动服务器 Start the server
 $server = new Server();
